890a2a98fe2b1fb950e32436957077ba836c42e2
ygursu <yasemingrs@gmail.com>
2024-02-05 00:10:22 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,50 @@
+﻿class dikdortgen
+{
+
+    public float Alan(float en, float boy)
+    {
+        return en * boy;
+    }
+
+    public float Cevre(float en, float boy)
+    {
+        return ((en + boy) * 2);
+    }
+
+}
+
+Program.cs:
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        dikdortgen dd = new dikdortgen();
+        Console.Write("Kısa kenarı Girin : ");
+        float kk = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Uzun kenarı Girin : ");
+        float uk = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Dikdörtgenin alanı : {0}", dd.Alan(kk, uk));
+        Console.WriteLine("Dikdörtgenin çevresi : {0}", dd.Cevre(kk, uk));
+        Console.ReadKey();
+    }
+}
+
+
+
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        dikdortgen dd = new dikdortgen();
+        Console.Write("Kısa kenarı Girin : ");
+        float kk = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Uzun kenarı Girin : ");
+        float uk = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Dikdörtgenin alanı : {0}", dd.Alan(kk, uk));
+        Console.WriteLine("Dikdörtgenin çevresi : {0}", dd.Cevre(kk, uk));
+        Console.ReadKey();
+    }
+}
+
